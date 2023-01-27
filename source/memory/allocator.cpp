2c186d7dde23a4220fb0b872b60e5b9a65cd6909
Cuking01 <cuking998244353@gmail.com>
2023-01-28 03:10:55 +0800

    修改内存分配器，分超级源，源，与最终分配器三类

--- source/memory/allocator.cpp
+++ source/memory/allocator.cpp
@@ -6,7 +6,7 @@
 namespace Mem
 {
 
-struct Source_T{};
+struct Source_T{};//此类型表示内部节点
 struct Thread_Safe_T{};
 
 //根据是否线程安全选择atomic还是非atomic
@@ -15,8 +15,8 @@ struct TS_helper{typedef U Type;};
 template<typename U>
 struct TS_helper<Thread_Safe_T,U>{typedef std::atomic<U> Type;};
 
-template<typename TS,typename SA>
-struct Allocator<Source_T,TS,SA>
+template<>
+struct Allocator<Source_T,Source_T,Thread_Safe_T>
 {
 	void*alloc(u3 sz){return Resource::alloc(sz);}
 	void free(void*p){Resource::free(p);}
@@ -25,18 +25,14 @@ struct Allocator<Source_T,TS,SA>
 //根内存分配器类型与根内存分配器
 RA ra;
 
-template<typename T,typename TS,typename SA>
-struct Allocator
+template<typename SA,typename TS>
+struct Allocator<Source_T,SA,TS>
 {
-	typedef T value_type;
-	typedef u3 size_type;
-	typedef s3 difference_type;
-	typedef std::true_type propagate_on_container_move_assignment;
-	typedef std::true_type propagate_on_container_copy_assignment;
+	typedef SA source_type;
+	static constexpr s2 is_thread_safe=std::is_same_v<Thread_Safe_T,TS>;
 
 	SA*source_allocator;
 	TS_helper<TS,u3>::Type in_use;
-
 	Allocator(SA&sa) noexcept :source_allocator(&sa),in_use(0){}
 	Allocator(const Allocator& o):source_allocator(o.source_allocator),in_use(0){}
 	Allocator(Allocator&& o):source_allocator(o.source_allocator),in_use(o.in_use){o.in_use=0;}
@@ -69,10 +65,48 @@ struct Allocator
 		in_use-=*(u3*)p;
 		source_allocator->free(p);
 	}
+};
+
+template<typename T,typename SA,typename TS>
+struct Allocator
+{
+	typedef T value_type;
+	typedef u3 size_type;
+	typedef s3 difference_type;
+	typedef std::true_type propagate_on_container_move_assignment;
+	typedef std::true_type propagate_on_container_copy_assignment;
+
+	typedef SA source_type;
+	static constexpr s2 is_thread_safe=std::is_same_v<Thread_Safe_T,TS>;
+
+	SA*source_allocator;
+	TS_helper<TS,u3>::Type in_use;
+
+	Allocator(SA&sa) noexcept :source_allocator(&sa),in_use(0){}
+	Allocator(const Allocator& o):source_allocator(o.source_allocator),in_use(0){}
+	Allocator(Allocator&& o):source_allocator(o.source_allocator),in_use(o.in_use){o.in_use=0;}
+	Allocator& operator=(const Allocator& o)
+	{
+		//if(in_use)Warning("fuzhi:xie lou!%llu\n",in_use);
+		source_allocator=o.source_allocator;
+		in_use=0;
+	}
+	Allocator& operator=(Allocator&& o)
+	{
+		//if(in_use)Warning("fuzhi:xie lou!%llu\n",in_use);
+		source_allocator=o.source_allocator;
+		in_use=o.in_use;
+		o.in_use=0;
+	}
+	~Allocator()
+	{
+		//if(in_use)Warning("xigou:xie lou!%llu\n",in_use);
+	}
+	
 
 	T* allocate(u3 n)
 	{
-		void*p=alloc(n*sizeof(T));
+		void*p=source_allocator->alloc(n*sizeof(T));
 		return (T*)((char*)p+al);
 	}
 
@@ -83,7 +117,7 @@ struct Allocator
 
 	void deallocate(T*p,u3 n)
 	{
-		free((void*)((char*)p-al));
+		source_allocator->free((void*)((char*)p-al));
 	}
 };
 
--- source/memory/allocator.h
+++ source/memory/allocator.h
@@ -5,11 +5,11 @@ namespace Mem
 	struct Source_T;
 	struct Thread_Safe_T;
 
-	//类型，线程安全标志，源分配器类型
-	template<typename T,typename TS,typename SA>
+	//类型，源分配器类型,线程安全标志
+	template<typename T,typename SA,typename TS=void>
 	struct Allocator;
 
-	typedef Allocator<Source_T,Thread_Safe_T,Source_T> RA;
+	typedef Allocator<Source_T,Source_T,Thread_Safe_T> RA;
 
 	extern RA ra;
 
